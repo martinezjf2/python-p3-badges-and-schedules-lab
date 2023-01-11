4a3a93c4ec4082d2d9d755a6981b152b8cfb830f
Jeffrey <martinezjf2@gmail.com>
2023-01-10 23:28:30 -0500

    Done

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,37 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return None
+    # sentences = []
+    # for name in names:
+    #    sentence =  badge_maker(name)
+    #    sentences.append(sentence)
+    # return sentences
+    sentences = [badge_maker(name) for name in names]
+    return sentences
 
 def assign_rooms(names):
-    return None
+    # sentences = []
+    # index = 1
+    # for name in names:
+    #     sentence = f"Hello, {name}! You'll be assigned to room {index}!"
+    #     sentences.append(sentence)
+    #     index += 1
+    # return sentences
+   
+    sentences = [i + 1 and f"Hello, {name}! You'll be assigned to room {i + 1}!" for (i, name) in enumerate(names)]
+    return sentences
+
+# Resource for the List Comprehension: https://tutorial.eyehunts.com/python/python-list-comprehension-multiple-variables-example-code/
 
 def printer(names):
-    return None
+
+    for sentence in batch_badge_creator(names):
+        print(sentence)
+
+    for sentence in assign_rooms(names):
+        print(sentence)
+    
+
+
+#Try using List Comprehensions for the last three methods
